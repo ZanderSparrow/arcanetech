474257116d78613b80b323f90c8b2fb5c6ad883d
Silverhawk <sondra.silverhawk@hush.com>
2017-01-31 19:02:22 -0800

    Display the entry titles

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import { connect } from 'react-redux';
+import { Card, CardSection } from './common';
 
 class LibraryList extends Component {
   render() {
-    return <View />
+    return (
+      <ScrollView>
+        <Card>
+          {this.props.library.map(entry => {
+            return (<CardSection key={entry.id}>
+              <Text>{entry.title}</Text>
+            </CardSection>)
+          })}
+        </Card>
+      </ScrollView>
+    );
   }
 };
 
 const mapStateToProps = state => {
-  console.log(state);
-  return state;
+  return { library: state.library };
 };
 
 export default connect(mapStateToProps)(LibraryList);
