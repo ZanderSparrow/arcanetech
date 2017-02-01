01b0041cefe2a6c5ff3a3438cc9536b18a35c03d
Silverhawk <sondra.silverhawk@hush.com>
2017-02-01 13:41:47 -0800

    Refactor LibraryEntry to a class-based component

--- src/components/LibraryEntry.js
+++ src/components/LibraryEntry.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { Card, CardSection } from './common';
 
-const LibraryEntry = ({library}) => {
-  let {title, description} = library;
+class LibraryEntry extends Component {
 
-  return (
-    <Card>
-      <CardSection>
-        <Text>{title}</Text>
-      </CardSection>
-        <Text>{description}</Text>
-      <CardSection>
-      </CardSection>
-    </Card>
-  );
+  render() {
+    let {title, description} = this.props.library;
+    return (
+      <Card>
+        <CardSection>
+          <Text style={styles.titleStyle}>{title}</Text>
+        </CardSection>
+        
+        <CardSection>
+          <Text>{description}</Text>
+        </CardSection>
+      </Card>
+    );
+  }
 }
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
 
 export default LibraryEntry;
